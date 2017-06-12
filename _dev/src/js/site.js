51e81518e76e52bc5b965eee1b6afbbd441e9683
Joern Bargmann <joernba@gmail.com>
2017-06-12 16:30:12 +0200

    added height function to text parent
    Signed-off-by: Joern Bargmann <joernba@gmail.com>

--- _dev/src/js/site.js
+++ _dev/src/js/site.js
@@ -1,3 +1,11 @@
+// timeout function to blend in pages
 setTimeout(function() {
   document.body.className = document.body.className.replace(" fade-out","");
 }, 300);
+// height function for absolute positioned elements
+// Bind to the resize event of the window object
+$(window).on("resize", function () {
+  var h = $(".js-height").height();
+  $(".js-assign-height").height(h);
+// Invoke the resize event immediately
+}).resize();
