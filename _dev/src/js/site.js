92bdc78db691e98a64b3f46ef21f2b0da7a53158
Joern Bargmann <joernba@gmail.com>
2017-06-21 14:40:12 +0200

    fixed height assign script
    Signed-off-by: Joern Bargmann <joernba@gmail.com>

--- _dev/src/js/site.js
+++ _dev/src/js/site.js
@@ -5,7 +5,7 @@ setTimeout(function() {
 // height function for absolute positioned elements
 // Bind to the resize event of the window object
 $(window).on("resize", function () {
-  var h = $(".js-height").height();
+  var h = $(".js-height:visible:first").height();
   $(".js-assign-height").height(h);
 // Invoke the resize event immediately
 }).resize();
